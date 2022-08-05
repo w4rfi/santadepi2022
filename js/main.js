5391695858ae6d1ee04d579b1ba6ed43ab108c3e
w4rfi <ignaciojc.95@hotmail.com>
2022-08-04 21:37:21 -0300

    eventos

--- js/main.js
+++ js/main.js
@@ -1,31 +1,90 @@
-const feminineZones = [{id: 1, zona: 'Bozo', precio: 500},
-                        {id: 2, zona: 'Rostro', precio: 900},
-                        {id: 3, zona: 'Axila', precio: 900},
-                        {id: 4, zona: 'Cavado', precio: 1200},
-                        {id: 5, zona: 'Cavado bikini', precio: 900}];
+class Zonas {
+    constructor (zona, precio, id, sexo) {
+        this.zona = zona;
+        this.precio = precio;
+        this.id = id;
+        this.sexo = sexo;
+    }
 
-const masculineZones = [{id: 1, zona: 'Pecho', precio: 900},
-                        {id:2, zona: 'Abdomen', precio: 900},
-                        {id: 3, zona: 'Espalda', precio: 900},
-                        {id: 4, zona: 'Gluteos', precio: 900},
-                        {id: 5, zona: 'Zona intima', precio: 1700}];
+    descuentoUno () {
+        return this.precio * 0.90     
+    }
 
-
-const costsFem = document.getElementById("addFeminineZones");
-const costsMasc = document.getElementById("addMasculineZones");
-
-for (const femininee of feminineZones) {
-    const feminineCosts = document.createElement("div")
-    feminineCosts.innerHTML = `<h3> Zona: ${femininee.zona} </h3>
-                                <p> Precio: <strong> ${femininee.precio} </strong><p>`;
-
-    costsFem.append(feminineCosts);               
+    descuentoDos () {
+        return this.precio * 0.75
+    }
 }
 
-for (const masculinee of masculineZones) {
-    const masculineCosts = document.createElement("div")
-    masculineCosts.innerHTML = `<h3> Zona: ${masculinee.zona} </h3>
-                                <p> Precio: <strong> ${masculinee.precio} </strong><p>`;
+const listaZonas = [];
 
-    costsMasc.append(masculineCosts);               
-}
+listaZonas.push( new Zonas("Bozo", 500, 1, "femenino"))
+listaZonas.push( new Zonas("Rostro", 900, 2, "femenino"))
+listaZonas.push( new Zonas("Axila", 900, 3, "femenino"))
+listaZonas.push( new Zonas("Cavado", 1200, 4, "femenino"))
+listaZonas.push( new Zonas("Cavado bikini", 900, 5, "femenino"))
+
+listaZonas.push( new Zonas("Pecho", 500, 6, "masculino"))
+listaZonas.push( new Zonas("Abdomen", 500, 7, "masculino"))
+listaZonas.push( new Zonas("Espalda", 500, 8, "masculino"))
+listaZonas.push( new Zonas("Gluteos", 500, 9, "masculino"))
+listaZonas.push( new Zonas("Zona intima", 500, 10, "masculino"))
+
+// for (const elemento of listaZonas) {
+//     console.log("id=", elemento.id)
+//     console.log("nombre=", elemento.zona)
+//     console.log("precio=", elemento.precio)
+//     console.log("sexo=", elemento.sexo)
+// }
+
+// const ocultar = () => {
+//     document.getElementById("addFeminineZones").style.display = 'none';
+// }
+
+
+// 
+
+
+const eventoClickfem = document.getElementById("fem");
+const eventoClickmasc = document.getElementById("masc");
+
+const costo = document.getElementById("costArea");
+
+// Evento para mostrar precios!
+
+eventoClickfem.addEventListener("click", () => {
+    costo.innerHTML = "";
+    for (const zonas of listaZonas) {
+        if (zonas.sexo != "masculino") {
+            const costs = document.createElement("div")
+            costs.innerHTML = `
+                        <h3> Zona: ${zonas.zona} </h3>               
+                        <p> Precio: <strong> ${zonas.precio} </strong><p>
+                        `;
+        costo.append(costs);
+        }
+    }
+    
+});
+
+eventoClickmasc.addEventListener("click", (e) => {
+    costo.innerHTML = "";
+    for (const zonas of listaZonas) {
+        if (zonas.sexo != "femenino") {
+            const costs = document.createElement("div")
+            costs.innerHTML = `
+                        <h3> Zona: ${zonas.zona} </h3>               
+                        <p> Precio: <strong> ${zonas.precio} </strong><p>
+                        `;
+        costo.append(costs);
+        }
+    }
+    
+});
+
+
+
+
+
+
+
+
